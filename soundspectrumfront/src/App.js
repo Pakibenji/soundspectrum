fb4bfaea5f5ef9ddc8ef1f61b1fc0e797ba789c1
Alcade <alcade.benjamin@gmail.com>
2023-06-12 10:33:22 +0200

    add scss settings

--- soundspectrumfront/src/App.js
+++ soundspectrumfront/src/App.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import User from "./components/User";
 import Radio from "./components/Radio";
-
 const App = () => {
   const [currUser, setCurrUser] = useState(null);
   return (
