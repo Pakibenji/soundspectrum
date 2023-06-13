6b83f4b31bfd74672faf02f2722426b204602e75
Alcade <alcade.benjamin@gmail.com>
2023-06-13 17:45:16 +0200

    fix adminpanel

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -4,14 +4,22 @@ RailsAdmin.config do |config|
   ### Popular gems integration
 
   ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
 
   ## == CancanCan ==
   # config.authorize_with :cancancan
-
+  config.authenticate_with do
+    # this is a rails controller helper
+    authenticate_or_request_with_http_basic('Login required') do |username, password|
+  
+      # Here we're checking for username & password provided with basic auth
+      resource = User.find_by(email: username)
+  
+      # we're using devise helpers to verify password and sign in the user 
+      if resource.valid_password?(password) && resource.role == "admin"
+        sign_in :admin, resource
+      end
+    end
+  end
   ## == Pundit ==
   # config.authorize_with :pundit
 
