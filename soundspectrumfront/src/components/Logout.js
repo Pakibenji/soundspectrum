a2bdf7bce8ef399d597f0be533a0c3662b9b6906
Alcade <alcade.benjamin@gmail.com>
2023-06-21 13:25:41 +0200

    restore logout

--- soundspectrumfront/src/components/Logout.js
+++ soundspectrumfront/src/components/Logout.js
@@ -1,29 +1,26 @@
 const Logout = ({ setCurrUser }) => {
-  // const logout = async (setCurrUser) => {
-  //   try {
-  //     const response = await fetch("http://localhost:3000/logout", {
-  //       method: "delete",
-  //       headers: {
-  //         "content-type": "application/json",
-  //         authorization: localStorage.getItem("token"),
-  //       },
-  //     });
-  //     const data = await response.json();
-  //     if (!response.ok) throw data.error;
-  //     localStorage.removeItem("token");
-  //     setCurrUser(null);
-  //     navigate("/");
-  //     alert("Vous êtes déconnecté");
-  //   } catch (error) {
-  //     console.log("error", error);
-  //     alert("Une erreur est survenue");
-  //   }
-  // };
+  const logout = async (setCurrUser) => {
+    try {
+      const response = await fetch("http://localhost:3000/logout", {
+        method: "delete",
+        headers: {
+          "content-type": "application/json",
+          authorization: localStorage.getItem("token"),
+        },
+      });
+      const data = await response.json();
+      if (!response.ok) throw data.error;
+      localStorage.removeItem("token");
+      setCurrUser(null);
+      alert("Vous êtes déconnecté");
+    } catch (error) {
+      console.log("error", error);
+      alert("Une erreur est survenue");
+    }
+  };
   const handleClick = (e) => {
+    logout(setCurrUser);
     e.preventDefault();
-    localStorage.removeItem("token");
-    setCurrUser(null);
-    alert("Vous êtes deconnecté");
   };
   return (
     <>
