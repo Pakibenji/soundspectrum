5a36bc67801a164d1993673301d4bb493a69ae90
Alcade <alcade.benjamin@gmail.com>
2023-06-22 14:03:53 +0200

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+    User.create!(email: "soundspectrum.office@gmail.com", password:"Thpalca@7491", role: "admin")
